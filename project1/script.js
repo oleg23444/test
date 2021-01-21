a6d903de1b0bc1da9b39aca1d4fdf2b7c79dcaaf
Oleg <stad.olegarch@gmail.com>
2021-01-21 10:07:54 +0200

    Первий сщььше

---
+++ project1/script.js
@@ -0,0 +1,21 @@
+'use strict'
+
+var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+console.log(second);
+console.log(leftBorderWidth);
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+console.log(persone["name"]);
+
+let arr = ['plum.png', 'orange.jpg', 'aplle.bmp'];
+console.log(arr[0]);
+//alert("Hello word!");
+// let answer = confirm("Are you here?");
+// console.log(answer);
+let answer = prompt("18+?","");
+console.log(typeof(arr));
